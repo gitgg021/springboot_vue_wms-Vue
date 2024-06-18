5c29c0778a7727d203184582e5ae7967c97e69f0
caoxuehan <caoxuehan@qq.com>
2024-06-18 23:55:08 +0800

    前端初始化调整基础样式

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="display: flex;line-height: 60px;height: 60px">
+    <div>
+      <i class="el-icon-s-fold" style="font-size: 20px;line-height: 65px"></i>
+    </div>
+    <div style="flex: 1;text-align: center;font-size: 34px;">
+      <span>欢迎来到仓库管理系统</span>
+    </div>
+    <span>王小虎</span>
+      <el-dropdown>
+        <i class="el-icon-arrow-down" style="margin-left: 5px;"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item @click.native="toUser">个人中心</el-dropdown-item>
+          <el-dropdown-item @click.native="logout">退出登录</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderPage",
+  methods:{
+    toUser(){
+      console.log('to_user')
+    },
+    logout(){
+      console.log('logout')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-container style="height: 100%; border: 1px solid #eee">
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246);height: 100%">
+      <AsidePage></AsidePage>
+    </el-aside>
+
+    <el-container style="height: 100%;">
+      <el-header style="text-align: right; font-size: 12px;height: 100%;border-bottom: rgba(30,0,0,0.66) 1px solid;">
+       <HeaderPage></HeaderPage>
+      </el-header>
+
+      <el-main style="height: 100%;">
+          <MainPage></MainPage>
+      </el-main>
+    </el-container>
+  </el-container>
+
+</template>
+
+<script>
+
+
+
+import AsidePage from "./Aside"
+import HeaderPage from "./Header"
+import MainPage from "./Main"
+
+export default {
+  name: "IndexPage",
+  components: {AsidePage,HeaderPage,MainPage},
+  }
+</script>
+
+<style scoped>
+.el-header {
+  #background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+.el-main{
+  padding: 5px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
